2cac7387a5e4165d2709445d8cbe0eb5fa115dd2
carnotweat <jasitis@gmail.com>
2023-06-24 10:02:13 +0530

    1

---
+++ README.md
@@ -0,0 +1,1 @@
+> ```  just follow the very staight forward [tutotial](https://binarin.ru/post/truly-reproducible-nixos/) , if you can manage that without messing your nix-profile and in worst case entire system.
--- configuration.nix
+++ configuration.nix
@@ -2,9 +2,24 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
-
+{ config, lib, pkgs, ... }:
+let
+  # config , nixpkgs version name ang git commit reference 
+  gitRepo      = "${toString pkgs.path}/../.git";
+  gitCommitId  = lib.substring 0 0 (lib.commitIdFromGitRepo gitRepo);
+  
+  # fonts
+# for derivation and attrsets
+  my-python-packages = ps: with ps; [
+  pip
+  fonttools
+  setuptools
+  afdko
+  ];
+in 
 {
+  system.nixos.versionSuffix = "-sameer-${gitCommitId}";
+  system.nixos.label = "sameer-${gitCommitId}";
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
@@ -15,7 +30,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -44,7 +59,45 @@
     LC_TELEPHONE = "en_IN";
     LC_TIME = "en_IN";
   };
+  nix = {
+  package = pkgs.nixFlakes;
+  extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+};
+  nix.nixPath = [
+  "nixpkgs=/etc/nixos/nixpkgs"
+  "nixos-config=/etc/nixos/configuration.nix"
+  ];
+  nixpkgs.overlays = [
+        (self: super: rec {
+      nyxt = super.nyxt.overrideAttrs (old: {
+        version = "3-pre-release-2";
+        installPhase = ''
+gappsWrapperArgs+=(
+        --set WEBKIT_FORCE_SANDBOX 0
+                )
+'' + old.installPhase;
+      });
+      lispPackages = super.lispPackages // {
+        nyxt = super.lispPackages.nyxt.overrideAttrs (old: rec {
+          version = "3-pre-release-2";
+          patches = [];
+          src = super.fetchFromGitHub {
+            owner = "atlas-engineer";
+            repo = "nyxt";
+            rev = "${version}";
+            sha256 = "0ancmbqpkzlnwp4g2f7gfwdpcb3mk8wsfrwsm87i168h8kn6bnj4";
+          };
+        });
+      };
+    })
+  ];
 
+ 
+
+  nix.settings.trusted-users = [ "root" "x" ];
+  
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
@@ -87,33 +140,63 @@
     description = "x";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      firefox
+      
+      #firefox
     #  thunderbird
+       
     ];
   };
+  security.sudo.wheelNeedsPassword = false;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
+  
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-  ];
-
+   agda
+   (agda.withPackages (with agdaPackages; [ standard-library ]))
+   idris
+   (idrisPackages.with-packages (with idrisPackages; [ contrib pruviloj ]))
+   python310
+   (pkgs.python3.withPackages my-python-packages)
+   (import (fetchTarball https://github.com/cachix/devenv/archive/v0.6.2.tar.gz)).default
+   emacs
+   ((emacsPackagesFor emacs).emacsWithPackages (epkgs:
+     [ epkgs.dash epkgs.agda2-mode ]))
+    nyxt
+    gnutls
+    gnumake
+    #gnupg
+    #pinentry
+    openssl_3
+    git
+    #libnma-gtk4
+    #sqlite
+    #mob
+    #pantalaimon
+    #maestral
+    direnv
+    nyxt
+    cachix
+    ispell
+    busybox
+    
+];
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+   programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+   };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
